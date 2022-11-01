2e5e107fcca69b4efb523041fd6fea8e4836631e
Russell Gordon <rgordon@lcs.on.ca>
2022-11-01 14:48:35 -0400

    Initial Commit

---
+++ NameBadge/NameBadgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NameBadgeApp.swift
+//  NameBadge
+//
+//  Created by Russell Gordon on 2022-11-01.
+//
+
+import SwiftUI
+
+@main
+struct NameBadgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
